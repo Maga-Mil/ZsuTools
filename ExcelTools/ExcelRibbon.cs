b3c5bfa9cab1fae823de93ba0579250791452cc1
Maga <305610805+Maga-Mil@users.noreply.github.com>
2026-07-28 21:36:33 +0300

    #1 Базова реалізація рапорта на премію. TODO обробка переміщень по посадах, обробка СЗЧ з табеля

--- ExcelTools/ExcelRibbon.cs
+++ ExcelTools/ExcelRibbon.cs
@@ -1,43 +1,53 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using ExcelDna.Integration;
 using ExcelDna.Integration.CustomUI;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
 using ZsuTools.GenerateTabel;
+using ZsuTools.Tables;
 using Application = Microsoft.Office.Interop.Excel.Application;
 
 namespace ZsuTools
 {
-    [ComVisible( true )]
+    [ComVisible(true)]
     public class ZsuToolsRibbon : ExcelRibbon
     {
         private IRibbonUI _ribbon;
 
         // Return the ribbon XML for Excel to render.
-        public override string GetCustomUI( string ribbonId )
+        public override string GetCustomUI(string ribbonId)
         {
-            var customUI =  @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui'>
+            var customUI = @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui'>
   <ribbon>
     <tabs>"
-                
+
 #if DEBUG
-            +"<tab id='tabZsuTools_DEBUG' label='ЗСУ debug'>"
+                           + "<tab id='tabZsuTools_DEBUG' label='ЗСУ debug'>"
 #else
             +"<tab id='tabZsuTools' label='ЗСУ'>"
 #endif
-                   + 
-      @"<group id='groupMain' label='ЄЖООС'>
-          <button id='btnAddReward' label='Рапорт на додаткову винагороду' size='large' imageMso='InternationalCurrency' showImage='true' onAction='OnAddRewardClicked' />
-        </group>
-        <group id='groupStroyova' label='Стройова'>
+                           +
+                           @"<group id='groupMain' label='ЄЖООС'>"
+                           + "<button id='btnAddReward' label='Рапорт на додаткову винагороду' size='large' imageMso='InternationalCurrency' showImage='true' onAction='OnAddRewardClicked' />"
+#if DEBUG
+                           + "<button id='btnPremReward' label='Рапорт на премію' size='large' imageMso='InternationalCurrency' showImage='true' onAction='OnPremRewardClicked' />"
+#endif
+                           + "</group>"
+                           +
+                           @"<group id='groupStroyova' label='Стройова'>
           <button id='btnGetTabelFromStroyovaBatch' label='Отримати табель зі Стройової Записки (пакетно)' size='large' imageMso='AccessListEvents' showImage='true' onAction='OnGetTabelFromStroyovaBatchClicked' />
 "
 // #if DEBUG
 //                    + 
 //         @"<button id='btnGetTabelFromStroyova' label='Отримати табель зі Стройової Записки' size='large' imageMso='AccessListEvents' showImage='true' onAction='OnGetTabelFromStroyovaClicked' />"
 // #endif
-                   +
-      @"</group>
+                           +
+                           @"</group>
       </tab>
     </tabs>
   </ribbon>
@@ -47,41 +57,132 @@ namespace ZsuTools
         }
 
         // Called when the ribbon is loaded by Excel
-        public void OnLoad( IRibbonUI ribbonUi )
+        public void OnLoad(IRibbonUI ribbonUi)
         {
             _ribbon = ribbonUi;
         }
 
-        public void OnAddRewardClicked( IRibbonControl control )
+        public void OnPremRewardClicked(IRibbonControl control)
         {
-            Application excelApp = (Application) ExcelDnaUtil.Application;
+            Application excelApp = (Application)ExcelDnaUtil.Application;
+            var workBook = excelApp.ActiveWorkbook;
+            foreach (Microsoft.Office.Interop.Excel.Worksheet workBookSheet in workBook.Sheets)
+            {
+                if (OOSTable.IsOOSTable(workBookSheet))
+                {
+                    //Load ООС table
+                    var oosTable = new OOSTable(workBookSheet);
+
+                    //Open folder dialog
+                    using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+                    {
+                        // Optional configuration
+                        folderDialog.Description = "Оберіть папку для збереження рапорту на премію";
+                        folderDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                        // Show the dialog and check if the user clicked OK
+                        if (folderDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            var selectedFolder = folderDialog.SelectedPath;
+                            
+                            //Create report base
+                            var reportPath = Path.Combine(selectedFolder, "рапорт_на_премію.docx");
+                            using (var document = DocX.Create(reportPath))
+                            {
+                                var defaultFont = new Font("Times New Roman");
+                                document.SetDefaultFont(defaultFont, 14);
+                                document.InsertParagraph("рапорт на премію");
+
+                                //Create table
+                                var table = document.AddTable(oosTable.Items.Count + 1, 7);
+                                //table.Design = Xceed.Words.NET.TableDesign.LightShadingAccent1;
+
+                                // Create autonumbered list for first column
+                                var numList = document.AddList(null, 0, ListItemType.Numbered, 1);
+                                for (int i = 0; i < oosTable.Items.Count; i++)
+                                {
+                                    document.AddListItem(numList,"");
+                                }
+                                
+                                //Add header
+                                table.Rows[0].Cells[0].Paragraphs[0].Append("№ п/п").FontSize(10);
+                                table.Rows[0].Cells[1].Paragraphs[0].Append("Військове звання").FontSize(10);
+                                table.Rows[0].Cells[2].Paragraphs[0].Append("Прізвище, ім’я,\nпо батькові").FontSize(10);
+                                table.Rows[0].Cells[3].Paragraphs[0].Append("Індекс посади").FontSize(10);
+                                table.Rows[0].Cells[4].Paragraphs[0].Append("РНОКПП").FontSize(10);
+                                table.Rows[0].Cells[5].Paragraphs[0].Append("Розмір премії").FontSize(10);
+                                table.Rows[0].Cells[6].Paragraphs[0].Append("Стягнення\n(ким, коли і за що накладено)").FontSize(10);
+
+                                //Add data
+                                for (int i = 0; i < oosTable.Items.Count; i++)
+                                {
+                                    var item = oosTable.Items[i];
+                                    var numberCell = table.Rows[i + 1].Cells[0];
+                                    numberCell.RemoveParagraphAt(0);
+                                    var listItem = numList.Items[i];
+                                    listItem.FontSize(10);
+                                    listItem.Append("").FontSize(10);
+                                    numberCell.InsertParagraph(listItem);
+                                    table.Rows[i + 1].Cells[1].Paragraphs[0].Append(item.Rank.RankName).FontSize(10);
+                                    table.Rows[i + 1].Cells[2].Paragraphs[0].Append(item.Person.ToString()).FontSize(10);
+                                    table.Rows[i + 1].Cells[3].Paragraphs[0]
+                                        .Append(item.Position.First().ToString() ?? "").FontSize(10);
+                                    table.Rows[i + 1].Cells[4].Paragraphs[0].Append(item.RNOKPP).FontSize(10);
+                                    table.Rows[i + 1].Cells[5].Paragraphs[0].Append("100%").FontSize(10);
+                                    table.Rows[i + 1].Cells[6].Paragraphs[0].Append("Відсутнє").FontSize(10);
+                                    
+                                }
+
+                                document.InsertTable(table);
+                                
+
+                                //Save document
+                                document.Save();
+                                
+                                Process.Start(new ProcessStartInfo(reportPath) { UseShellExecute = true });
+                            }
+                        }
+                    }
+
+
+                    return;
+                }
+            }
+
+            MessageBox.Show($"Не можу знайти таблицю ООС.", "ZSUTools", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+
+        public void OnAddRewardClicked(IRibbonControl control)
+        {
+            Application excelApp = (Application)ExcelDnaUtil.Application;
             try
             {
                 var workBook = excelApp.ActiveWorkbook;
                 //excelApp.DisplayStatusBar = true;
 
-                void UpdateStatusFunc( String status )
+                void UpdateStatusFunc(String status)
                 {
-                    UpdateStatus( excelApp, status );
+                    UpdateStatus(excelApp, status);
                 }
 
                 MoneyReportGenerator.CreateMoney_100_30_Report(workBook, UpdateStatusFunc);
             }
-            catch ( Exception e )
+            catch (Exception e)
             {
-                MessageBox.Show( $"Error catched, money report is discarded. {e}", "MoneyReport", MessageBoxButtons.OK,
-                        MessageBoxIcon.Error );
+                MessageBox.Show($"Error catched, money report is discarded. {e}", "MoneyReport", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             }
             finally
             {
-                excelApp.StatusBar        = false; 
+                excelApp.StatusBar = false;
                 //excelApp.DisplayStatusBar = false;
             }
         }
 
-        public void OnGetTabelFromStroyovaClicked( IRibbonControl control )
+        public void OnGetTabelFromStroyovaClicked(IRibbonControl control)
         {
-            Application excelApp = (Application) ExcelDnaUtil.Application;
+            Application excelApp = (Application)ExcelDnaUtil.Application;
             try
             {
                 var workBook = excelApp.ActiveWorkbook;
@@ -89,21 +190,22 @@ namespace ZsuTools
 
                 var stroyova = new СтройоваЗаписка(workBook);
             }
-            catch ( Exception e )
+            catch (Exception e)
             {
-                MessageBox.Show( $"Виникла помилка, створення табеля зі Стройової записки перервано. {e}", "Стройова записка", MessageBoxButtons.OK,
-                        MessageBoxIcon.Error );
+                MessageBox.Show($"Виникла помилка, створення табеля зі Стройової записки перервано. {e}",
+                    "Стройова записка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             }
             finally
             {
-                excelApp.StatusBar        = false; 
+                excelApp.StatusBar = false;
                 //excelApp.DisplayStatusBar = false;
             }
         }
-        
-        public void OnGetTabelFromStroyovaBatchClicked( IRibbonControl control )
+
+        public void OnGetTabelFromStroyovaBatchClicked(IRibbonControl control)
         {
-            Application excelApp = (Application) ExcelDnaUtil.Application;
+            Application excelApp = (Application)ExcelDnaUtil.Application;
             try
             {
                 var workBook = excelApp.ActiveWorkbook;
@@ -111,29 +213,30 @@ namespace ZsuTools
 
                 var batchResult = BatchProcessor.ProcessMonthlyFiles();
                 ResultTableGenerator.GenerateSummaryTable(batchResult);
-                MessageBox.Show($"Табель зі Стройової Записки сформовано успішно, оброблено {batchResult.Count} файлів.", "Стройова записка", MessageBoxButtons.OK, MessageBoxIcon.Information);        
+                MessageBox.Show(
+                    $"Табель зі Стройової Записки сформовано успішно, оброблено {batchResult.Count} файлів.",
+                    "Стройова записка", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            catch ( Exception e )
+            catch (Exception e)
             {
-                MessageBox.Show( $"Виникла помилка, створення табеля зі Стройової записки перервано. {e}", "Стройова записка", MessageBoxButtons.OK,
-                    MessageBoxIcon.Error );
+                MessageBox.Show($"Виникла помилка, створення табеля зі Стройової записки перервано. {e}",
+                    "Стройова записка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             }
             finally
             {
-                excelApp.StatusBar        = false; 
+                excelApp.StatusBar = false;
                 //excelApp.DisplayStatusBar = false;
             }
         }
-        
 
-        private void UpdateStatus( Application excelApp, string operationName )
+
+        private void UpdateStatus(Application excelApp, string operationName)
         {
             excelApp.StatusBar = $"{operationName} {UpdateStates[_updateStateIndex++ % UpdateStates.Length]}";
         }
 
-        private static readonly string[]    UpdateStates      = new []{"-", "\\", "|", "/"};
-        private                 int         _updateStateIndex = 0;
-
-
+        private static readonly string[] UpdateStates = new[] { "-", "\\", "|", "/" };
+        private int _updateStateIndex = 0;
     }
 }
---
+++ ExcelTools/Tables/OOSTable.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Office.Interop.Excel;
+using ZsuTools.Entities;
+
+namespace ZsuTools.Tables
+{
+    /// <summary>
+    /// Таблиця Облік Особового складу ЄЖООС
+    /// </summary>
+    public class OOSTable
+    {
+        public IReadOnlyList<Item> Items { get; private set; }
+        
+        public OOSTable( Worksheet ws)
+        {
+            if(ws == null) throw new ArgumentNullException(nameof(ws));
+            if(!IsOOSTable(ws))  throw new ArgumentException("Не знайдено таблицію ООС");
+
+            var firstRow = 6; //Hardcode
+            var lastRow = ExcelUtils.GetLastSingleNonEmptyCell(ws, 1);
+            
+            if(lastRow == null || lastRow.Row < firstRow)
+                throw new InvalidOperationException("Таблиця ООС не має даних");
+
+            var items = new List<Item>();
+            for (int i = 6; i < lastRow.Row; i++)
+            {
+                //Mandatory fields
+                String rankValue = ws.Cells[i, 1].Value2?.ToString();
+                if( string.IsNullOrEmpty(rankValue) )
+                    continue;
+                
+                String pibValue = ws.Cells[i, 2].Value2?.ToString();
+                if( string.IsNullOrEmpty(rankValue) )
+                    continue;
+
+                var rank = new Rank(rankValue);
+                var pib = new Person(pibValue);
+
+                String rnokpp = ws.Cells[i, 16].Value2?.ToString();
+                //Validate RNOKPP?
+                
+                var item = new Item
+                {
+                    Rank = rank,
+                    Person = pib,
+                    RNOKPP = rnokpp
+                };
+
+                string positionsString = ws.Cells[i, 3].Value2?.ToString();
+                if( !string.IsNullOrEmpty(positionsString) )
+                {
+                    var positions = positionsString.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var position in positions)
+                    {
+                        if(int.TryParse(position.Trim(), out int pos))
+                        {
+                            if(item.Position == null)
+                                item.Position = new List<int>();
+                            item.Position.Add(pos);
+                        }
+                    }
+                }
+
+                items.Add(item);
+                
+            }
+            
+            Items = items;
+        }
+        
+        public static bool IsOOSTable( Worksheet ws )
+        {
+            return ws.Name == "2. ООС"; 
+        }
+
+        public class Item
+        {
+            public Rank Rank { get; set; }
+            public Person Person { get; set; }
+            public List<int> Position { get; set; }
+            public string RNOKPP { get; set; }
+        }
+    }
+}
